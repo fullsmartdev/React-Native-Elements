b4f6aec3482c42b45248aa200ac3fba9e6b05b55
smartdevcode <senior.dev.software@gmail.com>
2016-09-11 08:29:23 +0400

    Enable syntax highlighting in README

--- src/config/fonts.js
+++ src/config/fonts.js
@@ -1,11 +1,12 @@
 export default {
   ios: {
-    regular: 'HelveticaNeue',
-    light: 'HelveticaNeue-Light',
-    lightItalic: 'HelveticaNeue-LightItalic',
-    bold: 'HelveticaNeue-Bold',
-    boldItalic: 'HelveticaNeue-BoldItalic',
-    black: 'HelveticaNeue-CondensedBlack',
+    regular: 'Lato',
+    light: 'Lato-Light',
+    lightItalic: 'Lato-LightItalic',
+    bold: 'Lato-Bold',
+    boldItalic: 'Lato-BoldItalic',
+    black: 'Lato-Black',
+    blackItalic: 'Lato-BlackItalic'
   },
   android: {
     regular: 'Roboto',
