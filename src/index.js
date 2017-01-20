7510bac7e8ff8e68c6de858843fcd4153285051f
smartdevcode <senior.dev.software@gmail.com>
2017-01-19 23:46:47 -0500

    Added Tile Component, Fixes #130

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ import normalize from './helpers/normalizeText'
 import Grid from './grid/Grid'
 import Row from './grid/Row'
 import Col from './grid/Col'
-import Slider from './slider/Slider'
+import Tile from './tile/Tile'
 
 const Elements = {
   Button,
@@ -47,7 +47,7 @@ const Elements = {
   Grid,
   Row,
   Col,
-  Slider
+  Tile,
 }
 
 module.exports = Elements
