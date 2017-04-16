12cb02e9fe4b0f7af05d7979cc0c50b3c80e9754
smartdevcode <senior.dev.software@gmail.com>
2017-04-16 02:36:35 -0400

    Fix propTypes

--- src/buttons/ButtonGroup.js
+++ src/buttons/ButtonGroup.js
@@ -12,7 +12,6 @@ const ButtonGroup = props => {
     selectedIndex,
     containerStyle,
     innerBorderStyle,
-    lastBorderStyle,
     buttonStyle,
     textStyle,
     selectedTextStyle,
@@ -45,7 +44,6 @@ const ButtonGroup = props => {
                   borderRightWidth: innerBorderStyle && innerBorderStyle.width || 1,
                   borderRightColor: innerBorderStyle && innerBorderStyle.color || colors.grey4
                 },
-                i === buttons.length - 1 && lastBorderStyle,
                 selectedIndex === i && {backgroundColor: selectedBackgroundColor || 'white'}
               ]}>
               <View style={[styles.textContainer, buttonStyle && buttonStyle]}>
@@ -108,7 +106,7 @@ ButtonGroup.propTypes = {
   onPress: PropTypes.func,
   buttons: PropTypes.array,
   containerStyle: View.propTypes.style,
-  textStyle: View.propTypes.style,
+  textStyle: NativeText.propTypes.style,
   selectedTextStyle: NativeText.propTypes.style,
   underlayColor: PropTypes.string,
   selectedIndex: PropTypes.number,
@@ -117,7 +115,6 @@ ButtonGroup.propTypes = {
   onShowUnderlay: PropTypes.func,
   setOpacityTo: PropTypes.any,
   innerBorderStyle: PropTypes.oneOf(View.propTypes.style, NativeText.propTypes.style),
-  lastBorderStyle: PropTypes.oneOf(View.propTypes.style, NativeText.propTypes.style),
   buttonStyle: View.propTypes.style,
   selectedBackgroundColor: PropTypes.string,
 };
