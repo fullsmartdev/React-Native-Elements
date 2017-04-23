009a8e40b1fd7b1abd909b8feefa0b4e732874ac
smartdevcode <senior.dev.software@gmail.com>
2017-04-24 00:51:34 +0530

    Fix for #320 #325 #323, fix propTypes across RNE (#326)

--- src/index.js
+++ src/index.js
@@ -25,7 +25,6 @@ import Col from './grid/Col';
 import Tile from './tile/Tile';
 import Slider from './slider/Slider';
 import Avatar from './avatar/Avatar';
-import Header from './header/Header';
 
 const Elements = {
   Button,
@@ -54,8 +53,7 @@ const Elements = {
   Col,
   Tile,
   Slider,
-  Avatar,
-  Header
+  Avatar
 };
 
 module.exports = Elements; // eslint-disable-line no-undef
