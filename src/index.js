4370b762ef2073a92e070acacc10382ff837428a
hideaki.chain@gmail.com <senior.dev.software@gmail.com>
2017-05-16 19:09:09 +0300

    Fix for checkbox to use event with iconRight equals true

--- src/index.js
+++ src/index.js
@@ -26,7 +26,8 @@ import Col from './grid/Col';
 import Tile from './tile/Tile';
 import Slider from './slider/Slider';
 import Avatar from './avatar/Avatar';
-import Header from './header/Header';
+import Rating from './rating/Rating';
+import SwipeDeck from './swipedeck/SwipeDeck';
 
 const Elements = {
   Badge,
@@ -57,7 +58,8 @@ const Elements = {
   Tile,
   Slider,
   Avatar,
-  Header
+  Rating,
+  SwipeDeck,
 };
 
 module.exports = Elements; // eslint-disable-line no-undef
